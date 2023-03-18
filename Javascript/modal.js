086100ade36771e6187b17b455b4add7f95aa76d
MrLuis <programmerlgp@gmail.com>
2023-03-18 18:44:13 -0400

    actualizacion de scripts y estilos

--- Javascript/modal.js
+++ Javascript/modal.js
@@ -6,7 +6,25 @@ document.querySelectorAll('.product-card').forEach(card => {
             if(card.getAttribute('id') === modal.getAttribute('data-target')){
                 $container.classList.add('active');
                 modal.classList.add('active');
-                const closeBtn = modal.children[0].children[0];
+
+                const imGroup = [...modal.children[2].childNodes[3].children];
+                const selectedImg = modal.children[2].childNodes[1].children[0].children[0];
+                let defaultImg = modal.children[2].childNodes[1].children[0].children[0].getAttribute('src')
+                console.log(defaultImg)
+                imGroup.forEach(image => {
+                    image.addEventListener('mouseover', (e) => {
+                        let src = e.target.getAttribute('src');
+                        selectedImg.setAttribute('src', src)
+                    });
+
+                    image.addEventListener('mouseout', () => {
+                        setTimeout(()=> {
+                            if(selectedImg.getAttribute('src')) selectedImg.setAttribute('src', defaultImg)
+                        },2500)
+                    })
+                });
+
+                const closeBtn = modal.children[0].children[1];
                 closeBtn.addEventListener('click', () => {
                     $container.classList.remove('active');
                     modal.classList.remove('active');
--- Javascript/slider.js
+++ Javascript/slider.js
@@ -1,23 +1,63 @@
-var swiper = new Swiper(".slide-content", {
-    slidesPerView: 3,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      dynamicBullets: true,
-    },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        520: {
-            slidesPerView: 2,
-        },
-        950: {
-            slidesPerView: 3,
-        },
-    },
-    keyboard: true,
-  });
+const swiper1 = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  navigation: {
+    nextEl: ".next-1",
+    prevEl: ".prev-1",
+  },
+  pagination: {
+    el: ".slide-1",
+    dynamicBullets: true,
+  },
+  breakpoints: {
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+  },
+  keyboard: true,
+});
+
+const swiper2 = new Swiper(".frida-slide", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".frida-pagination",
+    dynamicBullets: true
+  }
+});
+
+const swiper3 = new Swiper(".tiana-slide", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".tiana-pagination",
+    dynamicBullets: true
+  }
+});
+
+const swiper4 = new Swiper(".martina-slide", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".martina-pagination",
+    dynamicBullets: true
+  }
+});
+
+const swiper5 = new Swiper(".dafne-slide", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".dafne-pagination",
+    dynamicBullets: true
+  }
+});
+
+const swiper6 = new Swiper(".cirene-slide", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".cirene-pagination",
+    dynamicBullets: true
+  }
+});
